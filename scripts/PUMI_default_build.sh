7600ef3a63bb95e4921cf574c1332e3c6b5ca331
Morteza H. Siboni <hakimi1364@gmail.com>
2020-08-13 13:30:32 -0400

    Adds minor changes to the pumi_install script

--- scripts/PUMI_default_build.sh
+++ scripts/PUMI_default_build.sh
@@ -14,10 +14,10 @@ cd $REPO/cmbuild
 export PetraM=${TwoPiRoot}
 
 DO_TEST=false
-ENABLE_SIMMETRIX=OFF
-ENABLE_PARASOLID=OFF
-SIMMETRIX_LIB_DIR=''
-SIMMETRIX_MPI=openmpi
+# ENABLE_SIMMETRIX=OFF
+# ENABLE_PARASOLID=OFF
+# SIMMETRIX_LIB_DIR=''
+# SIMMETRIX_MPI=openmpi
 ENABLE_PYTHON=OFF
 while [[ $# -gt 0 ]]
 do
@@ -29,7 +29,8 @@ case $key in
     ;;
     --with-simmetrix)
     ENABLE_SIMMETRIX=ON
-    ENABLE_PARASOLID=ON	
+    ENABLE_PARASOLID=ON
+    ENABLE_FPP=ON
     shift # past argument    
     ;;
     --simmetrix-lib)
@@ -54,20 +55,20 @@ case $key in
 esac
 done
 
-export LIBRARY_PATH=/opt/SimModSuite/15.0-191017dev/lib:$LIBRARY_PATH
-export CPATH=/opt/SimModSuite/15.0-191017dev/include:$CPATH
+# export LIBRARY_PATH=/opt/SimModSuite/15.0-191017dev/lib:$LIBRARY_PATH
+# export CPATH=/opt/SimModSuite/15.0-191017dev/include:$CPATH
 
-export LD_LIBRARY_PATH=/opt/SimModSuite/15.0-191017dev/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/SimModSuite/15.0-191017dev/lib/acisKrnl
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/SimModSuite/15.0-191017dev/lib/psKrnl
+# export LD_LIBRARY_PATH=/opt/SimModSuite/15.0-191017dev/lib:$LD_LIBRARY_PATH
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/SimModSuite/15.0-191017dev/lib/acisKrnl
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/SimModSuite/15.0-191017dev/lib/psKrnl
 
-export CMAKE_PREFIX_PATH=/opt/SimModSuite/15.0-191017dev/:$CMAKE_PREFIX_PATH
-export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/opt/SimModSuite/15.0-191017dev/lib
-export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/opt/SimModSuite/15.0-191017dev/lib/acisKrnl
-export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/opt/SimModSuite/15.0-191017dev/lib/psKrnl
+# export CMAKE_PREFIX_PATH=/opt/SimModSuite/15.0-191017dev/:$CMAKE_PREFIX_PATH
+# export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/opt/SimModSuite/15.0-191017dev/lib
+# export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/opt/SimModSuite/15.0-191017dev/lib/acisKrnl
+# export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/opt/SimModSuite/15.0-191017dev/lib/psKrnl
 
-export SIMMETRIX_SIMMODSUITE_ROOT=/opt/SimModSuite/15.0-191017dev/
-export SIM_LICENSE_FILE=/home/shiraiwa/.simmetrix/MIT_Shiraiwa_2019
+# export SIMMETRIX_SIMMODSUITE_ROOT=/opt/SimModSuite/15.0-191017dev/
+# export SIM_LICENSE_FILE=/home/shiraiwa/.simmetrix/MIT_Shiraiwa_2019
 
 if $DO_TEST ;then
     cmake .. -DSCOREC_CXX_WARNINGS=OFF   \
@@ -78,17 +79,20 @@ if $DO_TEST ;then
              -DSCOREC_CXX_WARNINGS=OFF       \
              -DCMAKE_INSTALL_PREFIX="${TwoPiRoot}" \
              -DBUILD_SHARED_LIBS=on   \
+             -DMDS_ID_TYPE=long \
              -DCMAKE_VERBOSE_MAKEFILE=1 \
              -DIS_TESTING=ON \
+             -DBUILD_EXES=ON \
 	     -DENABLE_SIMMETRIX="${ENABLE_SIMMETRIX}" \
              -DSIM_PARASOLID="${ENABLE_PARASOLID}"   \
-	     -DSIMMETRIX_LIB_DIR="${SIMMETRIX_LIB_DIR}"  \
              -DSIM_MPI="${SIMMETRIX_MPI}" \
+             -DSIMMETRIX_LIB_DIR="${SIMMETRIX_LIB_DIR}"  \
+             -DENABLE_FPP="${ENABLE_FPP}" \
              -DPUMI_PYTHON_INTERFACE="${ENABLE_PYTHON}" \
              -DMESHES="${TwoPiRoot}"/src/PUMI-MESHES \
              -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-no-as-needed" \
              -DCMAKE_EXE_LINKER_FLAGS="-Wl,-no-as-needed" \
-             -DCMAKE_BUILD_TYPE="Debug" 
+             -DCMAKE_BUILD_TYPE="Debug"
 else
     cmake .. -DSCOREC_CXX_WARNINGS=OFF   \
              -DCMAKE_C_COMPILER="${MPICC}"    \
@@ -98,17 +102,20 @@ else
              -DSCOREC_CXX_WARNINGS=OFF       \
              -DCMAKE_INSTALL_PREFIX="${TwoPiRoot}" \
              -DBUILD_SHARED_LIBS=on   \
+             -DMDS_ID_TYPE=long \
+             -DIS_TESTING=OFF \
+             -DBUILD_EXES=OFF \
              -DSIM_MPI="${SIMMETRIX_MPI}" \
              -DENABLE_SIMMETRIX="${ENABLE_SIMMETRIX}" \
              -DSIM_PARASOLID="${ENABLE_PARASOLID}"   \
              -DSIMMETRIX_LIB_DIR="${SIMMETRIX_LIB_DIR}"  \
+             -DENABLE_FPP="${ENABLE_FPP}"   \
              -DPUMI_PYTHON_INTERFACE="${ENABLE_PYTHON}" \
              -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-no-as-needed" \
              -DCMAKE_EXE_LINKER_FLAGS="-Wl,-no-as-needed" \
-             -DCMAKE_VERBOSE_MAKEFILE=1 
+             -DCMAKE_VERBOSE_MAKEFILE=1
 fi
 echo "${MAKEOPT}"
-make "${MAKEOPT}"
 make install
 
 # add link to pyCore.py. it seems like there is two possible locations
